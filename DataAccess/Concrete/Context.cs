1bfd85256c809b48757a4d3b37a92b5ba5042ad2
Mehmet ALBAYRAK <mehmet__albayrak@outlook.com>
2022-03-06 22:14:11 +0300

    added migrations

---
+++ DataAccess/Concrete/Context.cs
@@ -0,0 +1,27 @@
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class Context : DbContext
+    {
+        // override onconfig + tab
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=(localdb)\MSSQLLocalDB;database=CoreBlogDb; integrated security=true;");
+            //base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
